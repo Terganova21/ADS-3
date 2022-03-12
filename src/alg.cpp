e2b133a4b8e09141e09f653dd005b9e9fd758467
Victoria Terganova <94222399+Terganova21@users.noreply.github.com>
2022-03-12 20:42:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,26 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int l = 0;
+  int r = size - 1;
+  int mid;
+  int count = 0;
+  while (l <= r) {
+    mid = (l + r) / 2;
+    if (*(arr + mid) == value) {
+      count++;
+      break;
+    }
+    if (*arr + mid) < value)
+      l = mid - 1;
+    if (*(arr + mid) > value)
+      r = mid + 1;
+  }
+  if (count != 0) {
+    for (int k = mid + 1; *(arr + k) == value; k++)
+      count++;
+    for (int k = mid - 1; *(arr + k) == value; k--)
+      count++;
+  }
+  return count;
 }
